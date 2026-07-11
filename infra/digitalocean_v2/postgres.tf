bd82670be88c6f2aebb294f8a00f5300561be4a8
Shawn Carrick <carrickshawn@gmail.com>
2026-07-11 14:39:32 -0400

    feat: stub out PG DB TF support

---
+++ infra/digitalocean_v2/postgres.tf
@@ -0,0 +1,14 @@
+resource "digitalocean_database_cluster" "postgres" {
+  name       = var.postgres_name
+  engine     = "pg"
+  version    = var.postgres_version
+  size       = var.postgres_size
+  region     = var.postgres_region
+  node_count = var.postgres_node_count
+  project_id = digitalocean_project.project.id
+}
+
+resource "digitalocean_database_db" "rails" {
+  cluster_id = digitalocean_database_cluster.postgres.id
+  name       = var.postgres_database_name
+}
